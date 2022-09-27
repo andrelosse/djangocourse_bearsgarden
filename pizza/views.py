9cfd457fd45533d65f50125b6c604e2451baf7be
andrelosse <andreloze@gmail.com>
2022-09-27 20:36:24 -0300

    Created Django form for pizza ordering

--- pizza/views.py
+++ pizza/views.py
@@ -1,7 +1,19 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render
+from .forms import PizzaForm
 
 def index(request):
     return render(request, 'index.html')
 
 def order(request):
-    return render(request, 'order.html')
+
+    if request.method != 'POST':
+        form = PizzaForm()
+        return render(request, 'order.html', {'pizzaform':form})
+    
+    note = NULL
+    filled_form = PizzaForm(request.POST)
+    
+    if filled_form.is_valid():
+        note = 'Order completed!'
+        return render(request, 'order.html', {'pizzaform':filled_form, 'note':note})
