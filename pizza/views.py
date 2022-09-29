fbf229aeedc6c8cc7dfb9254db0a127ddf2f9219
andrelosse <andreloze@gmail.com>
2022-09-28 21:02:58 -0300

    Added option to edit order

--- pizza/views.py
+++ pizza/views.py
@@ -3,6 +3,7 @@ from textwrap import fill
 from django.shortcuts import render
 from .forms import PizzaForm, MultiplePizzas
 from django.forms import formset_factory
+from .models import Pizza
 
 def index(request):
     return render(request, 'index.html')
@@ -19,8 +20,14 @@ def order(request):
     filled_form = PizzaForm(request.POST)
     
     if filled_form.is_valid():
+        created_pizza = filled_form.save()
+        created_pizza_pk = created_pizza.id
         note = 'Order completed!'
-        return render(request, 'order.html', {'pizzaform':filled_form, 'note':note, 'multiple_form':multiple_form})
+        return render(request, 'order.html', {
+            'pizzaform':filled_form, 
+            'note':note, 
+            'multiple_form':multiple_form, 
+            'created_pizza_pk':created_pizza_pk })
 
 def pizzas(request):
     number_pizzas = 2
@@ -44,3 +51,18 @@ def pizzas(request):
     
     else:
         return render(request, 'pizzas.html', {'formset':formset})
+
+def edit_order(request, pk):
+
+    note = ''
+    pizza = Pizza.objects.get(pk=pk)
+    form = PizzaForm(instance=pizza)
+
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST, instance=pizza)
+        if filled_form.is_valid():
+            filled_form.save()
+            form = filled_form
+            note = 'Order updated!'
+
+    return render(request, 'edit_order.html', {'pizzaform':form, 'pizza':pizza, 'note':note})
